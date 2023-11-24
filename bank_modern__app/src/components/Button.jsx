95b525d3e3869c4e407d7cb7c868eb8b586760d4
Nikita Zytsev <eronmcgonagal@gmail.com>
2023-11-24 14:27:34 +0300

    feat: add CardDeal section

--- bank_modern__app/src/components/Button.jsx
+++ bank_modern__app/src/components/Button.jsx
@@ -1,5 +1,12 @@
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ styles }) => {
+  return (
+    <button
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}
+      type="button"
+    >
+      Get Started
+    </button>
+  );
 };
 
 export default Button;
